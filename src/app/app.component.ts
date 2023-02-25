746a87687d7cad7dcc8c714748d56f191b68b049
1Neeraj0 <1neeraj0singh@gmail.com>
2023-02-25 10:18:12 +0530

    send

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DialogBodyComponent } from './dialog-body/dialog-body.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'dialog-box';
+  constructor(private matDialog: MatDialog) {}
+  openDialog() {
+    this.matDialog.open(DialogBodyComponent);
+  }
+}
